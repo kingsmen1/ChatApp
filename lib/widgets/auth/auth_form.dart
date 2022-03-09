afbd77b632dfceb834dcc93401bf5c32bc6d42f7
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-03-09 22:03:34 +0530

    FORM VALIDATION

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class AuthForm extends StatefulWidget {
   @override
@@ -25,108 +27,142 @@ class _AuthFormState extends State<AuthForm> {
     }
   }
 
+  //form password validator
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'password is required'),
+    MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
+    MaxLengthValidator(12,
+        errorText: 'password should not be more than  8 digits long'),
+    PatternValidator(r'(?=.*?[#?!@$%^&*-_])',
+        errorText: 'passwords must have at least one special character')
+  ]);
+
+  //form email validator
+  final emailValidator = MultiValidator([
+    RequiredValidator(errorText: '*Required*'),
+    EmailValidator(errorText: 'Email is no Valid!')
+  ]);
+
+  final UserNameValidator = MultiValidator([
+    RequiredValidator(errorText: '*Required*'),
+    MinLengthValidator(4,
+        errorText: 'Username should be atlest 4 characters long')
+  ]);
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Card(
         margin: const EdgeInsets.all(20),
         child: SingleChildScrollView(
-            padding: EdgeInsets.all(16),
-            child: Form(
+          padding: EdgeInsets.all(16),
+          child: Form(
               key: _formKey,
               child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-              TextFormField(
-              key: ValueKey('Email'),
-              validator: (value) {
-                if (value == null || value
-                    .trim()
-                    .isEmpty) {
-                  return 'Please enter your email address';
-                }
-                // Check if the entered email has the right format
-                if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-                  return 'Please enter a valid email address';
-                }
-                // Return null if the entered email is valid
-                return null;
-              },
-              decoration: const InputDecoration(
-                labelText: 'Email address',
-              ),
-              keyboardType: TextInputType.emailAddress,
-              onSaved: (value) {
-                _userEmail = value;
-              },
-            ),
-              if (!_isLogin)
-              TextFormField(
-                key: ValueKey('UserName'),
-                validator: (value) {
-                  if (value == null || value
-                      .trim()
-                      .isEmpty) {
-                    return 'Enter a UserName';
-                  }
-                  if (value
-                      .trim()
-                      .length < 4) {
-                    return 'UserName must be Atleast 4 characters long';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                ),
-                onSaved: (value) {
-                  _userName = value;
-                },
-              ),
-
-              TextFormField(
-                key: ValueKey('password'),
-                validator: (value) {
-                  if (value == null || value
-                      .trim()
-                      .isEmpty) {
-                    return 'Enter Password';
-                  }
-                  if (value
-                      .trim()
-                      .length < 7) {
-                    return 'Password must be atleast 7 Characters long';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-                onSaved: (value) {
-                  _userPassword = value;
-                },
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              ElevatedButton(
-                  onPressed: _trySubmit,
-                  child: Text(_isLogin ? 'Login' : 'SignUp')),
-              TextButton(
-                child: Text(_isLogin
-                    ? 'Create New Account'
-                    : 'I already have an Account'),
-                onPressed: () {
-                  setState(() {
-                    _isLogin = !_isLogin;
-                    print(_isLogin);
-                  });
-                },
-              ),
-              ],
-            )),
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    autofillHints: [AutofillHints.email],
+                    key: ValueKey('Email'),
+                    validator: emailValidator,
+                    /* (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Please enter your email address';
+                      }
+                      // Check if the entered email has the right format
+                      if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                        return 'Please enter a valid email address';
+                      }
+                      // Return null if the entered email is valid
+                      return null;
+                    },*/
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        hintText: 'Email address',
+                        prefixIcon: Icon(Icons.email)),
+                    keyboardType: TextInputType.emailAddress,
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
+                  ),
+                  if (!_isLogin)
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      key: ValueKey('UserName'),
+                      validator: UserNameValidator,
+                      /*(value) {
+                        MinLengthValidator(4, errorText: 'userName should be atleast 4 Character');
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Enter a UserName';
+                        }
+                        // if (value.trim().length < 4) {
+                        //   return 'UserName must be Atleast 4 characters long';
+                        // }
+                        return null;
+                      },*/
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          labelText: 'Username',
+                          prefixIcon: Icon(Icons.account_circle)),
+                      onSaved: (value) {
+                        _userName = value;
+                      },
+                    ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    key: ValueKey('password'),
+                    validator: passwordValidator,
+                    /*(value) {
+                    //   if (value == null || value.trim().isEmpty) {
+                    //     return 'Enter Password';
+                    //   }
+                    //   if (value.trim().length < 7) {
+                    //     return 'Password must be atleast 7 Characters long';
+                    //   }
+                    //   return null;
+                    /},*/
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.password)),
+                    obscureText: true,
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
+                    onChanged: (value) => _userPassword = value,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ElevatedButton(
+                      onPressed: _trySubmit,
+                      child: Text(_isLogin ? 'Login' : 'SignUp')),
+                  TextButton(
+                    child: Text(_isLogin
+                        ? 'Create New Account'
+                        : 'I already have an Account'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                        print(_isLogin);
+                      });
+                    },
+                  ),
+                ],
+              )),
+        ),
       ),
-    ),);
+    );
   }
 }
