5aec577ae26240a52143430b0dd19a6e4f415288
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-03-12 02:26:29 +0530

    334

--- lib/widgets/chats/messages.dart
+++ lib/widgets/chats/messages.dart
@@ -1,28 +1,32 @@
 import 'package:chatapp/widgets/chats/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance
-          .collection('chat')
-          .orderBy('createdAt' , descending: true)
-          .snapshots(),
-      builder: (ctx, AsyncSnapshot<QuerySnapshot> ChatsnapShot) {
-        if (ChatsnapShot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        final document = ChatsnapShot.data.docs;
-        //ListView.builder default takes full width
-        return ListView.builder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createdAt', descending: true)
+            .snapshots(),
+        builder: (ctx, AsyncSnapshot<QuerySnapshot> ChatsnapShot) {
+          if (ChatsnapShot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final ChatsDoc = ChatsnapShot.data.docs;
+          final userId = FirebaseAuth.instance.currentUser.uid;
+          //ListView.builder default takes full width
+          return ListView.builder(
             reverse: true,
-            itemCount: document.length,
-            itemBuilder: (ctx, i) => MessageBubble(document[i]['text']));
-      },
-    );
+            itemCount: ChatsDoc.length,
+            itemBuilder: (ctx, i) => MessageBubble(
+                ChatsDoc[i]['text'], ChatsDoc[i]['userId'] == userId,
+                key: ValueKey(ChatsDoc[i].id)),
+          );
+        });
   }
 }
